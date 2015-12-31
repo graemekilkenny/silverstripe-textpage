fede9125f14f8ec687f00ebf9b845fcecf0508ed
helpfulrobot <chris@silverstripe.com>
2016-01-01 00:29:34 +1300

    Converted to PSR-2

--- code/TextPage.php
+++ code/TextPage.php
@@ -1,23 +1,23 @@
 <?php
-class TextPage extends Page {
+class TextPage extends Page
+{
 
-	private static $db = array(
-	);
+    private static $db = array(
+    );
 
-	private static $has_one = array(
-	);
-
+    private static $has_one = array(
+    );
 }
-class TextPage_Controller extends Page_Controller {
+class TextPage_Controller extends Page_Controller
+{
 
-	public function init() {
-      	if(Director::fileExists(project() . "/css/textpage.css")) {
-         	Requirements::css(project() . "/css/textpage.css");
-      	} else {
-         	Requirements::css("TextPage/css/textpage.css");
-      	}
-      	parent::init();   
+    public function init()
+    {
+        if (Director::fileExists(project() . "/css/textpage.css")) {
+            Requirements::css(project() . "/css/textpage.css");
+        } else {
+            Requirements::css("TextPage/css/textpage.css");
+        }
+        parent::init();
     }
-	
-
 }
